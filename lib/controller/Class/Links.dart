279bd183b030bb03eca8a19e4d1b8b0ea78c98af
ibrahimhajras <hjres333@gmail.com>
2024-01-07 02:36:33 +0300

    m

--- lib/controller/Class/Links.dart
+++ lib/controller/Class/Links.dart
@@ -1,4 +1,4 @@
-const String LinkServerName = "http://192.168.1.11/project1";
+const String LinkServerName = "http://192.168.1.27/project1";
 const String LinkLogin = "$LinkServerName/Auth/login.php";
 const String LinkSingup = "$LinkServerName/Auth/SignUp.php";
 const String LinkReports = "$LinkServerName/Home/Report.php";
