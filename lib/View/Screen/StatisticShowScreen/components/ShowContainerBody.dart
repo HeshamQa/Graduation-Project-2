543bc7d49088bbe2ee27b509fb1551de21907195
HeshamQa <qaoudqa@gmail.com>
2024-01-20 22:32:13 +0300

    Hesham

--- lib/View/Screen/StatisticShowScreen/components/ShowContainerBody.dart
+++ lib/View/Screen/StatisticShowScreen/components/ShowContainerBody.dart
@@ -19,25 +19,48 @@ class ShowContainerBody extends StatefulWidget {
 class _ShowContainerBodyState extends State<ShowContainerBody> {
   @override
   void initState() {
-    Provider.of<StatisticsProvider>(context,listen: false).fetchStatistics(widget.year, widget.crop, widget.city, gender);
+    getData();
     super.initState();
   }
+
+  getData() {
+    Provider.of<StatisticsProvider>(context, listen: false).fetchStatistics(
+        widget.year, widget.crop, widget.city, gender);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Consumer<StatisticsProvider>(
-      builder: (BuildContext context, StatisticsProvider value, Widget? child) => Column(
-        children: [
-          ShowDetails(textType: '${S.of(context).Crops} : ', textDet: widget.crop,),
-          const Divider(),
-          ShowDetails(textType: '${S.of(context).Year} : ', textDet: widget.year),
-          const Divider(),
-          ShowDetails(textType: '${S.of(context).CityName} : ', textDet: widget.city),
-          const Divider(),
-          ShowDetails(textType: '${S.of(context).CultivatedArea} : ', textDet: value.statistic[0].Space),
-          const Divider(),
-          ShowDetails(textType: '${S.of(context).TotalProduction} : ', textDet: value.statistic[0].Production)
-        ],
-      ),
+      builder: (BuildContext context, StatisticsProvider value, Widget? child) {
+        if (value.statistic.isEmpty) {
+          // Show loading indicator or placeholder
+          return const Center(child: CircularProgressIndicator());
+        }
+        // Once data is available, build your widgets
+        return Column(
+          children: [
+            ShowDetails(textType: '${S
+                .of(context)
+                .Crops} : ', textDet: widget.crop,),
+            const Divider(),
+            ShowDetails(textType: '${S
+                .of(context)
+                .Year} : ', textDet: widget.year),
+            const Divider(),
+            ShowDetails(textType: '${S
+                .of(context)
+                .CityName} : ', textDet: widget.city),
+            const Divider(),
+            ShowDetails(textType: '${S
+                .of(context)
+                .CultivatedArea} : ', textDet: value.statistic[0].Space),
+            const Divider(),
+            ShowDetails(textType: '${S
+                .of(context)
+                .TotalProduction} : ', textDet: value.statistic[0].Production)
+          ],
+        );
+      },
     );
   }
 }
